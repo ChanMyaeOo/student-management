ab05f6ac4659743df25aab2559c759f71e49ec34
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2020-12-08 12:25:25 +0630

    added table for student list

--- src/components/showcase/Showcase.js
+++ src/components/showcase/Showcase.js
@@ -2,10 +2,40 @@ import React from 'react';
 import './showcase.css';
 
 const Showcase = () => {
-    const student = null;
+    const student = true;
 
     if(student) {
-        return <div className="showcase__list">Student Lists</div>
+        return <div className="showcase__list">
+            <h1>Student List</h1>
+            <table className="table">
+                <tr className="table__header">
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Parent</th>  
+                    <th>Address</th>
+                </tr>
+                <tr>
+                    <td>Kyaw Kyaw</td>
+                    <td>09976754676</td>
+                    <td>U Hla Myint</td>
+                    <td>Aung Mingalar</td>
+                </tr>
+
+                 <tr>
+                    <td>Kyaw Kyaw</td>
+                    <td>09976754676</td>
+                    <td>U Hla Myint</td>
+                    <td>Aung Mingalar</td>
+                </tr>
+
+                 <tr>
+                    <td>Kyaw Kyaw</td>
+                    <td>09976754676</td>
+                    <td>U Hla Myint</td>
+                    <td>Aung Mingalar</td>
+                </tr>
+            </table>
+        </div>
     }
 
     return (
